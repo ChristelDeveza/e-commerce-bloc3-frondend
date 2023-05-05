6b846bee0bf473bc68f06294edc1be00ee70527f
ChristelDeveza <chris.dvz66@gmail.com>
2023-05-05 11:49:48 +0200

    form + axios request to add a product

--- src/components/CreateProduct.jsx
+++ src/components/CreateProduct.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import axios from 'axios';
+import { useState } from 'react';
 
 function CreateProduct() {
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    axios
+      .post('http://localhost:8080/products', {
+        name,
+        price,
+      })
+      .then((res) => console.log(res.data));
+  }
+
   return (
-    <div>CreateProduct</div>
-  )
+    <div>
+      <h1>Ajouter un produit</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor='name'>Libellé du produit : </label>
+          <input
+            type='text'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor='price'>Prix : </label>
+          <input
+            type='number'
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+        </div>
+        <button type='button' onClick={handleSubmit}>
+          ajouter
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default CreateProduct
+export default CreateProduct;
