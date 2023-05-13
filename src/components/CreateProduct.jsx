1cc843f569507ac078cd55cf9d13a7583c28d0f8
ChristelDeveza <chris.dvz66@gmail.com>
2023-05-13 18:20:15 +0200

    request categories list

--- src/components/CreateProduct.jsx
+++ src/components/CreateProduct.jsx
@@ -1,9 +1,11 @@
 import axios from 'axios';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function CreateProduct() {
   const [name, setName] = useState('');
   const [price, setPrice] = useState('');
+  const [categoryId, setCategoryId] = useState();
+  const [categories, setCategories] = useState([]);
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -12,11 +14,18 @@ function CreateProduct() {
       .post('http://localhost:8080/products', {
         name,
         price,
+        category: { id : categoryId },
       })
       .then((res) => console.log(res.data));
   }
 
-  
+  useEffect(() => {
+    axios
+      .get('http://localhost:8080/categories')
+      .then((res) => setCategories(res.data))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div>
       <h1>Ajouter un produit</h1>
@@ -36,6 +45,20 @@ function CreateProduct() {
             value={price}
             onChange={(e) => setPrice(e.target.value)}
           />
+        </div>
+        <div>
+          <label htmlFor='category'>Catégorie : </label>
+          <select
+            value={categoryId}
+            onChange={(e) => setCategoryId(e.target.value)}
+          >
+            <option value=''>Sélectionnez une catégorie</option>
+            {categories.map((category) => (
+              <option key={category.id} value={category.category_id}>
+                {category.libelle}
+              </option>
+            ))}
+          </select>
         </div>
         <button type='button' onClick={handleSubmit}>
           Ajouter
