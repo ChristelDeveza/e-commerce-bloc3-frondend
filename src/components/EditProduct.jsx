9d88be1e348ee9c0a7f6c38e99d09b3de06a00e6
ChristelDeveza <chris.dvz66@gmail.com>
2023-05-17 17:10:01 +0200

    update a product without image

--- src/components/EditProduct.jsx
+++ src/components/EditProduct.jsx
@@ -5,7 +5,10 @@ import { useParams } from 'react-router-dom';
 function EditProduct() {
   const [name, setName] = useState('');
   const [price, setPrice] = useState('');
-  const { id } = useParams();
+  const [description, setDescription] = useState('');
+  const [categoryId, setCategoryId] = useState();
+  const [categories, setCategories] = useState([]);
+    const { id } = useParams();
 
   useEffect(() => {
     // Get request by id
@@ -14,18 +17,28 @@ function EditProduct() {
       .then((res) => {
         setName(res.data.name);
         setPrice(res.data.price);
+        setDescription(res.data.description)
       })
       .catch((err) => console.log(err));
   }, [id]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:8080/categories')
+      .then((res) => setCategories(res.data))
+      .catch((error) => console.log(error));
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     // PUT request to update product
     axios
-      .put(`http://localhost:8080/products/${id}`, {
+      .put(`http://localhost:8080/update/${id}`, {
         name,
         price,
+        description,
+        categoryId
       })
       .then((res) => console.log(res))
       .catch((err) => console.log(err));
@@ -54,6 +67,28 @@ function EditProduct() {
             value={price}
             onChange={(e) => setPrice(e.target.value)}
           />
+        </div>
+        <div>
+          <label htmlFor='description'>Description du produit : </label>
+          <input
+            type='text'
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor='category'>Catégorie : </label>
+          <select
+            value={categories.id}
+            onChange={(e) => setCategoryId(e.target.value)}
+          >
+            <option value=''>Sélectionnez une catégorie</option>
+            {categories.map((category) => (
+              <option key={category.id} value={category.id}>
+                {category.libelle}
+              </option>
+            ))}
+          </select>
         </div>
         <button type='submit'>Modifier</button>
       </form>
