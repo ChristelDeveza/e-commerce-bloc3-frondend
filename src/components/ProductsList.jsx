20cd2cb53e24dd111a6f2c3831676bcfe8758bd7
ChristelDeveza <chris.dvz66@gmail.com>
2023-05-19 20:48:39 +0200

    filters

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -6,6 +6,8 @@ import photo from '../../../commercial/src/main/resources/static/photo/56/heathe
 
 function ProductsList() {
   const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [selectedCategories, setSelectedCategories] = useState([]);
   console.log(products);
   // const url = "../../../commercial/src/main/resources/static"
 
@@ -14,36 +16,101 @@ function ProductsList() {
       .get('http://localhost:8080/products')
       .then((res) => setProducts(res.data));
   }, []);
+
+  // useEffect(() => {
+  //   axios
+  //     .get('http://localhost:8080/products')
+  //     .then((res) => console.log("test", res.data.filter((el)=>el.category?.libelle === "Alimentation")))
+  // }, []);
+
+  const handleCategoryFilter = (category) => {
+    if (selectedCategories.includes(category)) {
+      const updatedCategories = selectedCategories.filter(
+        (c) => c !== category
+      );
+      setSelectedCategories(updatedCategories);
+
+      const filtered = products.filter((product) =>
+        updatedCategories.includes(product.category?.libelle)
+      );
+      setFilteredProducts(filtered);
+    } else {
+      const updatedCategories = [...selectedCategories, category];
+      setSelectedCategories(updatedCategories);
+
+      const filtered = products.filter((product) =>
+        updatedCategories.includes(product.category?.libelle)
+      );
+      setFilteredProducts(filtered);
+    }
+  };
+
   return (
     <div className='container'>
       {' '}
       <h1>Catalogues de nos produits</h1>
+      <button onClick={() => handleCategoryFilter('Alimentation')}>
+        {selectedCategories.includes('Alimentation')
+          ? 'Annuler filtre Alimentation'
+          : 'Filtrer Alimentation'}
+      </button>
+      <button onClick={() => handleCategoryFilter('Vêtement')}>
+        {selectedCategories.includes('Vêtement')
+          ? 'Annuler filtre Vêtement'
+          : 'Filtrer Vêtement'}
+      </button>
       <ul>
-        {products.map((product) => (
-          <>
-            <li className='card' key={product.product_id}>
-              <div className='card-image'>
-                <DeleteProduct product={product} />
-                <img src={photo} alt='picture' />
+        {filteredProducts.length > 0
+          ? filteredProducts.map((product) => (
+              <>
+                <li className='card' key={product.product_id}>
+                  <div className='card-image'>
+                    <DeleteProduct product={product} />
+                    <img src={photo} alt='picture' />
 
-                {/* <img src={`../../../commercial/src/main/resources/static${product.photosImagePath}`} alt='picture' /> */}
-              </div>
-              <div className='note'>{product.category?.libelle}</div>
-              <div className='card-title'>
-                {product.name} <br />
-                <span className='price'>
-                  {product.price} €{' '}
-                  <span className='discount-price'>
-                    {' '}
-                    {product.discountedPrice != null &&
-                      `| Promo ${product.discountedPrice} € (-${product.discount?.percentage} %)`}
-                  </span>
-                </span>
-              </div>
-              <div className='card-description'>{product.description}</div>
-            </li>
-          </>
-        ))}
+                    {/* <img src={`../../../commercial/src/main/resources/static/${product.photosImagePath}`} alt='picture' /> */}
+                  </div>
+                  <div className='note'>{product.category?.libelle}</div>
+                  <div className='card-title'>
+                    {product.name} <br />
+                    <span className='price'>
+                      {product.price} €{' '}
+                      <span className='discount-price'>
+                        {' '}
+                        {product.discountedPrice != null &&
+                          `| Promo ${product.discountedPrice} € (-${product.discount?.percentage} %)`}
+                      </span>
+                    </span>
+                  </div>
+                  <div className='card-description'>{product.description}</div>
+                </li>
+              </>
+            ))
+          : products.map((product) => (
+              <>
+                <li className='card' key={product.product_id}>
+                  <div className='card-image'>
+                    <DeleteProduct product={product} />
+                    <img src={photo} alt='picture' />
+
+                    {/* <img src={`../../../commercial/src/main/resources/static${product.photosImagePath}`} alt='picture' /> */}
+                  </div>
+                  <div className='note'>{product.category?.libelle}</div>
+                  <div className='card-title'>
+                    {product.name} <br />
+                    <span className='price'>
+                      {product.price} €{' '}
+                      <span className='discount-price'>
+                        {' '}
+                        {product.discountedPrice != null &&
+                          `| Promo ${product.discountedPrice} € (-${product.discount?.percentage} %)`}
+                      </span>
+                    </span>
+                  </div>
+                  <div className='card-description'>{product.description}</div>
+                </li>
+              </>
+            ))}
       </ul>
     </div>
   );
